c03b06fcba6e4a891d099eaf0e3d17c1e4a29e8e
ruben <rubenski0345@gmail.com>
2020-04-17 08:17:59 +0200

    Geeft de informatie over de klant en zet in zijn korting in een tostring

--- Testing/src/F1/Klant.java
+++ Testing/src/F1/Klant.java
@@ -1,12 +1,28 @@
 package F1;
 
 public class Klant {
-	private String naam;
-	private double Kortingspercentage;
+	String naam;
+	double kortingsPercentage;
 	
 	public Klant(String nm) {
 		naam = nm;
+
 		
 	}
 	
+	public double getKorting() {
+		return kortingsPercentage;
+	}
+	
+	public void setKorting(double kP) {
+		kortingsPercentage = kP;
+	}
+	
+	public String toString() {
+		if (naam == null) {
+			naam = "onbekende huurder";
+		}
+		String k = "op naam van: " + naam + "(korting: " + kortingsPercentage + "%)";
+		return k;
+	}
 }
