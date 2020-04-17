40befe41679bcd7add9a283a0494ada0c53dc6a7
ruben <rubenski0345@gmail.com>
2020-04-17 08:19:06 +0200

    Geeft de informatie over gehele autohuur en zet de klant en auto tostring in zijn eigen tostring

--- Testing/src/F1/Autohuur.java
+++ Testing/src/F1/Autohuur.java
@@ -1,10 +1,57 @@
 package F1;
 
 public class Autohuur {
-
-	private int aantalDagen;
-	
-	public Autohuur() {
+	Integer aantalDagen;
+	Auto auto;
+	Klant klant;
 		
+	public void setGehuurdeAuto(Auto gA) {
+		auto = gA;
+	}
+		
+	public void setHuurder(Klant k) {
+		klant = k;
+	}
+		
+	public void setAantalDagen(Integer aD) {
+	    aantalDagen = aD;
+	}
+	
+	public Auto getGehuurdeAuto() {
+		return auto;
+	}
+	
+	public Klant getHuurder() {
+		return klant;
+	}
+	
+	public double totaalPrijs() {
+		Double totaalprijs = aantalDagen*auto.prijsPerDag - aantalDagen*auto.prijsPerDag*(klant.kortingsPercentage/100);
+		return totaalprijs;
+	}
+
+	
+	public String toString() {
+		String a = "";	
+        if (getGehuurdeAuto() == null) {
+        	a = "\t er is geen auto bekend \n";
+        } else {
+        	a = "\t autotype: " + getGehuurdeAuto().toString() + "\n";
+        }
+
+        if (getHuurder() == null) {
+            a = a + "\t er is geen huurder bekend \n";
+        } else {
+            a = a + "\t " + getHuurder().toString() + "\n";
+        }
+
+        if (this.aantalDagen == null) {
+            a = a + "\t aantal dagen: 0 en dat kost 0.0 \n";
+        } else {
+            a = a + "\t aantal dagen: " + this.aantalDagen + " en dat kost " + totaalPrijs() + "\n";
+        }
+        
+		return a;
+
 	}
 }
