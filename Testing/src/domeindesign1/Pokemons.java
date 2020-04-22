ab4d6748b8ea41c938e78b98870f69d8d9e456d7
ruben <rubenski0345@gmail.com>
2020-04-22 11:06:52 +0200

    Getters en setters

--- Testing/src/domeindesign1/Pokemons.java
+++ Testing/src/domeindesign1/Pokemons.java
@@ -1,5 +1,41 @@
 package domeindesign1;
 
 public class Pokemons {
+	private String pokemonnaam;
+	private String type;
+	
+	public Pokemons(String pn, String tp) {
+		type = tp;
+		pokemonnaam = pn;
+	}
+	
+	public String getPokemonnaam() {
+		return pokemonnaam;
+	}
 
+	public void setPokemonnaam(String pn) {
+		this.pokemonnaam = pn;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String tp) {
+		this.type = tp;
+	}
+
+	public String toString() {
+		String pokemon =  pokemonnaam + " (" + type + " type)" ;
+		return pokemon;
+	}
 }
+
+	
+	
+	
+	
+	
+	
+	
+
--- Testing/src/domeindesign1/Rol.java
+++ Testing/src/domeindesign1/Rol.java
@@ -1,5 +1,42 @@
 package domeindesign1;
 
 public class Rol {
+	private boolean gymleader;
+	private boolean trainer;
+	
+	public Rol(boolean gl, boolean tn) {
+		gymleader = gl;
+		trainer = tn;
+		
+	}
 
+	public boolean isGymleader() {
+		return gymleader;
+	}
+
+	public void setGymleader(boolean gymleader) {
+		this.gymleader = gymleader;
+	}
+
+	public boolean isTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(boolean trainer) {
+		this.trainer = trainer;
+	}
+	
+	public String toString() {
+		String rol = "";
+		if (gymleader == true){
+			rol =  "is een gymleader";
+		}
+		else if (trainer == true){
+			rol =  "is een trainer";
+		}
+		else{
+			rol =  "is een npc";	
+		}
+		return rol;
+	}
 }
