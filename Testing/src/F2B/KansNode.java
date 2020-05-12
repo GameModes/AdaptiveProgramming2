486e95820bf36da8a0753b3da4a04af1c7e31c55
ruben <rubenski0345@gmail.com>
2020-05-12 15:04:54 +0200

    Heeft de variabelen en constructors

---
+++ Testing/src/F2B/KansNode.java
@@ -0,0 +1,39 @@
+package F2B;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class KansNode {
+	private String naam;
+	private boolean endstate;
+	ArrayList<String> path = new ArrayList<String>();
+	HashMap<String, KansNode> locatiesprong = new HashMap<String, KansNode>();
+
+
+	public KansNode(String nm, boolean end) {
+		naam = nm;
+		endstate = end;
+		
+	}
+
+
+	public void setVerbinding(int i, KansNode k2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
