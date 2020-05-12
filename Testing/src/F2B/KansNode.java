7f7441a4581750b5b178ef7ad87d334e9e101ea7
ruben <rubenski0345@gmail.com>
2020-05-12 15:28:44 +0200

    Heeft Dobbelsteennummer functie om de nummer te bereiken

--- Testing/src/F2B/KansNode.java
+++ Testing/src/F2B/KansNode.java
@@ -1,13 +1,14 @@
 package F2B;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.HashMap;
 
 public class KansNode {
 	private String naam;
 	private boolean endstate;
 	ArrayList<String> path = new ArrayList<String>();
-	HashMap<String, KansNode> locatiesprong = new HashMap<String, KansNode>();
+	HashMap<Integer, KansNode> locatiesprong = new HashMap<Integer, KansNode>();
 
 
 	public KansNode(String nm, boolean end) {
@@ -17,13 +18,23 @@ public class KansNode {
 	}
 
 
-	public void setVerbinding(int i, KansNode k2) {
-		// TODO Auto-generated method stub
+	public void setVerbinding(int kansnummer, KansNode node) {
+		locatiesprong.put(kansnummer, node);
 		
 	}
 	
 	
-	
+	public ArrayList<String> Dobbelsteennummer(ArrayList<String> input) {
+		if(endstate != true) {
+			int randomNum = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+			path = locatiesprong.get(randomNum).Dobbelsteennummer(input);
+			path.add(0, naam);
+			return input;
+		}
+		else {
+			return input;
+		}
+	}
 	
 	
 	
