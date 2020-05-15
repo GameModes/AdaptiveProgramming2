1662ec3380a5b3e466d4a626e52a99edfc032195
ruben <rubenski0345@gmail.com>
2020-05-15 10:36:36 +0200

    Is ingekort tot alleen een while loop

--- Testing/src/F2B/Dobbelsteen.java
+++ Testing/src/F2B/Dobbelsteen.java
@@ -3,35 +3,16 @@ package F2B;
 public class Dobbelsteen {
 	//List nodes
 	
-	
+		
 	
 	public static String bouwDobbelsteen(KansNode node) {
 		KansNode currentNode = node;
+		
 		while (currentNode.getEndState() == false) {
-			System.out.println("test");
-			if (currentNode.getEndState() == true) {
-				
-				break;
-			}
-			else {
-				currentNode = currentNode.NextLocatie();
-			}
+			currentNode = currentNode.NextLocatie();
+			
 		}
-	if (currentNode.getEndState() == true) {
+
 		return currentNode.getNodeNaam();
 	}
-	else {
-		return "Bij elkaar niet 1";
-	}
-	
-	
-	
-	
-	}
-	//Bouwdobbelsteen(aantalogen):
-	// for loop door range zijdes
-		//maak voor elk zijde een node
-		//zet elke zijde nummer in een array
-	// for loop n door arraylist
-		//S1 maakt verbinding voor N
 }
