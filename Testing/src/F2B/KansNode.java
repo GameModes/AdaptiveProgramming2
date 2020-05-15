1b93823b31c4acdb88443dadf3d647623f8a17d1
ruben <rubenski0345@gmail.com>
2020-05-15 10:35:22 +0200

    Sorted Hashmap en key van value

--- Testing/src/F2B/KansNode.java
+++ Testing/src/F2B/KansNode.java
@@ -2,13 +2,18 @@ package F2B;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.lang.Math; 
 import java.util.Map;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class KansNode {
 	ArrayList<KansNode> kanzen = new ArrayList<KansNode>();
 	HashMap<KansNode, Double> locatiesprong = new HashMap<KansNode, Double>();
-	private double random = Math.random();
 	private String nodeNaam;
 	private Boolean endState;
 
@@ -38,7 +43,7 @@ public class KansNode {
 
 	public void setVerbinding(KansNode nummer, double kansnummer) {
 		locatiesprong.put(nummer, kansnummer);
-
+		
 	}
 	
 
@@ -46,29 +51,42 @@ public class KansNode {
 	public KansNode NextLocatie() {
 			
 		  Double random3 = Math.random(); 
-		  System.out.println("Randomgetal" + random3);
 		  Double lagerdan = 0.0;
 		  
-		  KansNode volgendenode = null;
+	       List list = new LinkedList(locatiesprong.entrySet()); //sort hashmap by values
+	       Collections.sort(list, new Comparator() {
+	            public int compare(Object o1, Object o2) {
+	               return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+	            }
+	       });
+
+	       HashMap sortedHashMap = new LinkedHashMap();
+	       for (Iterator it = list.iterator(); it.hasNext();) {
+	              Map.Entry entry = (Map.Entry) it.next();
+	              sortedHashMap.put(entry.getKey(), entry.getValue());
+	       } 
+	       locatiesprong = sortedHashMap;
+		  //Source: https://beginnersbook.com/2013/12/how-to-sort-hashmap-in-java-by-keys-and-values/
+		  
+		  
+		  
+		  
+		  KansNode volgendenode = null; //Vind de volgende node met random
 		  boolean gevondenlager = false;
-		  		System.out.println("de list" +locatiesprong);
 		      for(int c = 0; c < locatiesprong.size(); c++){
 		    	  Double locatievalue = locatiesprong.get(locatiesprong.keySet().toArray()[c]);
-		    	  System.out.println("getallen in de list" + locatievalue);
 		    	  if(locatievalue > random3 && gevondenlager == false){
 		    		  lagerdan = locatievalue;
 		    		  gevondenlager = true;
 		    		  
 		    	  }
-		          for(Map.Entry entry: locatiesprong.entrySet()){
-		              if(lagerdan.equals(entry.getValue())){
-		            	  volgendenode = (KansNode) entry.getKey();
+		          for(Map.Entry x: locatiesprong.entrySet()){ //Krijg de key van de gevonden value
+		              if(lagerdan.equals(x.getValue())){
+		            	  volgendenode = (KansNode) x.getKey();
 		                  break;
 		              }
 		          }
 		      }
-		      System.out.println("Gekozen getal: " +lagerdan);
-		      System.out.println("Volgendenode: " + volgendenode);
 		      return volgendenode;
 		}
 
