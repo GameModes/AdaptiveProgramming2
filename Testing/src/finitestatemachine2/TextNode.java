14df0bd9177501b818c796cf00c6f8be9b1bc285
ruben <rubenski0345@gmail.com>
2020-05-15 21:43:58 +0200

    Bijna complete nextlocatie functie met character als input

--- Testing/src/finitestatemachine2/TextNode.java
+++ Testing/src/finitestatemachine2/TextNode.java
@@ -2,38 +2,92 @@ package finitestatemachine2;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TextNode {
 	private String naam;
 	ArrayList<String> path = new ArrayList<String>();
 	HashMap<String, TextNode> locatiesprong = new HashMap<String, TextNode>();
+	private String bekekenLetter;
+	private TextNode currentNode;
 	
-	public TextNode(String nm) {
+	public TextNode getCurrentNode() {
+		return currentNode;
+	}
+
+	public void setCurrentNode(TextNode currentNode) {
+		this.currentNode = currentNode;
+	}
+
+	public String getBekekenletter() {
+		return bekekenLetter;
+	}
+
+	public void setBekekenletter(String bekekenletter) {
+		this.bekekenLetter = bekekenletter;
+	}
+
+	public TextNode(String nm) {	
 		naam = nm;
+	}
+
+	public ArrayList<String> getPath() {
+		return path;
+	}
+
+	public void setPath(ArrayList<String> path) {
+		this.path = path;
+	}
+
+	public HashMap<String, TextNode> getLocatiesprong() {
+		return locatiesprong;
+	}
+
+	public void setLocatiesprong(HashMap<String, TextNode> locatiesprong) {
+		this.locatiesprong = locatiesprong;
 	}
 
 	public void setVerbinding(String letter, TextNode node) {
 		locatiesprong.put(letter, node);
 	}
-
-	public ArrayList<String> FiniteStateMachine(String input){
-        if(input.length() != 0){
-        	String bekekenletter = input.substring(0, 1); //pakt de 1ste letter, https://www.w3schools.com/jsref/jsref_substring.asp
-            String overig = input.substring(1);
-            if(locatiesprong.containsKey(bekekenletter)){ //checkt of waarde in hashmap bestaat
-                path = locatiesprong.get(bekekenletter).FiniteStateMachine(overig); //run nog een keer met de overige letters
-                path.add(0, naam);
-                return path;
+	
+	public TextNode nextLocatie(char input) {
+		String stringInput = String.valueOf(input);
+		if(locatiesprong.containsKey(stringInput)){ //checkt of waarde in hashmap bestaat
+			currentNode = locatiesprong.get(stringInput);
+			TextNode node = getCurrentNode().locatiesprong.get(stringInput);
             } else {//zo niet dan
-                throw new IllegalArgumentException("bij " + naam + " " + bekekenletter + " niet mogelijk is.");
+                throw new IllegalArgumentException("bij " + naam + " " + bekekenLetter + " niet mogelijk is.");
             }
             
-        } else { //Dus het einde
-            return new ArrayList<>() {{
-                add(naam);
-            }};
-        }
+         return currentNode;
+        
 	}
+	
+	
+//		  Double random = Math.random(); 
+//		  Double lagerdan = 0.0;
+//		  locatieSprong =  mapSorter(locatieSprong);
+//		  DobbelNode volgendenode = null; //Vind de volgende node met random
+//		  
+//		      for(int c = 0; c < locatieSprong.size(); c++){
+//		    	  Double locatievalue = locatieSprong.get(locatieSprong.keySet().toArray()[c]);
+//		    	  if(locatievalue > random){
+//		    		  lagerdan = locatievalue;
+//		    		  break;
+//		    		  
+//		    	  }
+//		      }
+//		          for(Map.Entry x: locatieSprong.entrySet()){ //Krijg de key van de gevonden value
+//		              if(lagerdan.equals(x.getValue())){
+//		            	  volgendenode = (DobbelNode) x.getKey();
+//		                  break;
+//		              }
+//		          
+//		      }
+//		      return volgendenode;
+//		}
+	
 }
 
 	
