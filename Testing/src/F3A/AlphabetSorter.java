eec2da314b97dc63fed4cb8eaa22207128d81ece
ruben <rubenski0345@gmail.com>
2020-05-21 12:32:27 +0200

    Alphabetlist aangemaakt en oproepbaar

--- Testing/src/F3A/AlphabetSorter.java
+++ Testing/src/F3A/AlphabetSorter.java
@@ -1,5 +1,12 @@
 package F3A;
+import java.util.*;
 
 public class AlphabetSorter {
+	List<String> arrayList = new ArrayList<String>();
+	public AlphabetSorter(ArrayList aL) {
+		arrayList = aL;
+		
 
+	}
+	
 }
--- Testing/src/F3A/Main.java
+++ Testing/src/F3A/Main.java
@@ -1,5 +1,14 @@
 package F3A;
+import java.util.*;
 
 public class Main {
+	public static void main(String[] args) {
+	ArrayList<String> alphabet1 = new ArrayList<String>();
+	alphabet1.add("x");
+	alphabet1.add("c");
+	alphabet1.add("b");
+	alphabet1.add("h");
+	AlphabetSorter sortedAlphabetList1 = new AlphabetSorter(alphabet1);	
+	}
 	
 }
