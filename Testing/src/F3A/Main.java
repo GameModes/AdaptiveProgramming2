c50275ef7a1f3c0acd95b838533aca8c3a4928b9
ruben <rubenski0345@gmail.com>
2020-05-22 12:23:34 +0200

    Main heeft extra input

--- Testing/src/F3A/Main.java
+++ Testing/src/F3A/Main.java
@@ -3,21 +3,29 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
+	ArrayList<ArrayList<Character>> charList = new ArrayList<ArrayList<Character>>();
+	AlphabetSorter sortedAlphabetList = new AlphabetSorter();
+
 	ArrayList<String> alphabet1 = new ArrayList<String>();
-	ArrayList<ArrayList<Character>> chartest = new ArrayList<ArrayList<Character>>();
+	alphabet1.add("xe");
 	alphabet1.add("xb");
-	alphabet1.add("ce");
 	alphabet1.add("bt");
 	alphabet1.add("hi");
-	AlphabetSorter sortedAlphabetList1 = new AlphabetSorter(alphabet1);
+
 	
-	chartest = sortedAlphabetList1.charListsMaker(alphabet1);
-//	System.out.println(sortedAlphabetList1.charListsMaker(alphabet1));
-	System.out.println(chartest);
-	chartest = sortedAlphabetList1.bubbleSort(chartest);
-	System.out.println(chartest);
+	ArrayList<String> alphabet2 = new ArrayList<String>();
+	alphabet2.add("ik");
+	alphabet2.add("wil");
+	alphabet2.add("een");
+	alphabet2.add("koekje");
 	
-	alphabet1 = sortedAlphabetList1.compareTomethod(alphabet1);
+	charList = sortedAlphabetList.charListsMaker(alphabet2);
+	charList = sortedAlphabetList.bubbleSort(charList);
+	alphabet2 = sortedAlphabetList.stringListsMaker(charList);
+	System.out.println(alphabet2);
+	
+	
+	alphabet1 = sortedAlphabetList.compareTomethod(alphabet1);
 	System.out.println(alphabet1);
 	
 	
