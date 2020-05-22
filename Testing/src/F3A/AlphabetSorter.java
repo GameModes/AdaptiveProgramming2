d9ea003eb5d860b350776f2067d335f47ca9d734
ruben <rubenski0345@gmail.com>
2020-05-22 12:01:46 +0200

    Methode1 werkend & Methode2 bijna af

--- Testing/src/F3A/AlphabetSorter.java
+++ Testing/src/F3A/AlphabetSorter.java
@@ -3,10 +3,64 @@ import java.util.*;
 
 public class AlphabetSorter {
 	List<String> arrayList = new ArrayList<String>();
-	public AlphabetSorter(ArrayList aL) {
+	public AlphabetSorter(ArrayList<String> aL) {
 		arrayList = aL;
 		
 
 	}
 	
+	public ArrayList<String> compareTomethod (ArrayList<String> aL){ //methode1
+		
+	      for(int i = 0; i<aL.size()-1; i++) {
+	          for (int j = i+1; j<aL.size(); j++) {
+	             if(aL.get(i).compareTo(aL.get(j))>0) {
+	                String temp = aL.get(i);
+	                aL.set(i, aL.get(j));
+	                aL.set(j, temp);
+	             }
+	          }
+	       }
+		return aL;
+		
+	}	
+	
+	public ArrayList<ArrayList<Character>> charListsMaker (ArrayList<String> aL){//methode 2, hakt list in character stukjes
+		ArrayList<ArrayList<Character>> wordList = new ArrayList<ArrayList<Character>>();
+		for(int index = 0; index<aL.size(); index++) {
+			String word = aL.get(index);
+			ArrayList<Character> charList = new ArrayList<Character>();
+			for (int ci = 0; ci < word.length(); ci++){
+			    charList.add(word.charAt(ci));
+			}
+			wordList.add(charList);
+
+		}
+		return wordList;
+	}
+	
+	public ArrayList<ArrayList<Character>> bubbleSort (ArrayList<ArrayList<Character>> wordList) { //methode 2, vergelijkt en vervangt
+
+	int lengte = wordList.size();
+	while (lengte > 0) {
+		
+		lengte = lengte - 1;
+		for(int index = 0; index<wordList.size(); index++) {
+			try { //tegen de out of bounds error
+			ArrayList<Character> firstword = wordList.get(index);
+			ArrayList<Character> secondword = wordList.get(index+1);
+			if (firstword.get(0) > secondword.get(0)) {
+				ArrayList<Character> temp = wordList.get(index);
+				wordList.set(index, wordList.get(index+1));
+				wordList.set(index+1, temp);  }
+			} catch(Exception e) {
+				break;
+			}
+			
+		}
+	}
+		return wordList;
+		
+	}
+
+	public ArrayList<String> stringListsMaker
 }
--- Testing/src/F3A/Main.java
+++ Testing/src/F3A/Main.java
@@ -4,11 +4,23 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 	ArrayList<String> alphabet1 = new ArrayList<String>();
-	alphabet1.add("x");
-	alphabet1.add("c");
-	alphabet1.add("b");
-	alphabet1.add("h");
-	AlphabetSorter sortedAlphabetList1 = new AlphabetSorter(alphabet1);	
+	ArrayList<ArrayList<Character>> chartest = new ArrayList<ArrayList<Character>>();
+	alphabet1.add("xb");
+	alphabet1.add("ce");
+	alphabet1.add("bt");
+	alphabet1.add("hi");
+	AlphabetSorter sortedAlphabetList1 = new AlphabetSorter(alphabet1);
+	
+	chartest = sortedAlphabetList1.charListsMaker(alphabet1);
+//	System.out.println(sortedAlphabetList1.charListsMaker(alphabet1));
+	System.out.println(chartest);
+	chartest = sortedAlphabetList1.bubbleSort(chartest);
+	System.out.println(chartest);
+	
+	alphabet1 = sortedAlphabetList1.compareTomethod(alphabet1);
+	System.out.println(alphabet1);
+	
+	
 	}
 	
 }
