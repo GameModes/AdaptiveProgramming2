beaf65b38bdfa61790ed70a25a9e6aded2a6f5d1
ruben <rubenski0345@gmail.com>
2020-05-29 17:50:45 +0200

    AutoRit werkend met Stadnamen

--- Testing/src/dijkstraPath3/App.java
+++ Testing/src/dijkstraPath3/App.java
@@ -3,20 +3,20 @@ package dijkstraPath3;
 public class App {
 
 	public static void main(String[] args) {
-        Plaats v1 = new Plaats("A");
-        Plaats v2 = new Plaats("B");
-        Plaats v3 = new Plaats("C");
+        Plaats v1 = new Plaats("Amersfoort");
+        Plaats v2 = new Plaats("Culemborg");
+        Plaats v3 = new Plaats("Beuschium");
 
-        v1.addNeighbour(new AutoRit(1, v1, v2));
-        v1.addNeighbour(new AutoRit(10, v1, v2));
-
-        v2.addNeighbour(new AutoRit(1, v2, v3));
+        v1.addNeighbour(new AutoRit(2, v1, v2));
+        v1.addNeighbour(new AutoRit(2, v1, v3));
+        v2.addNeighbour(new AutoRit(2, v2, v3));
+        
 
         Dijkstra dijkstra = new Dijkstra();
         dijkstra.computePath(v1);
 
         System.out.println(dijkstra.getShortestPathTo(v3));
-        System.out.println(dijkstra.getShortestPathTo(v3));
+
     }
 
 	}
